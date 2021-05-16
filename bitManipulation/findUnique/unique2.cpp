82a98f2cf3a29c72d941eef6db4e3d079cab622f
mr-robot-007 <anujgusain108@gmail.com>
2021-05-16 10:02:58 +0530

    try commit

---
+++ bitManipulation/findUnique/unique2.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int getBit(int n,int pos){
+    return (n & (1<< pos)!=0);
+}
+void unique(int arr[], int n)
+{
+    int xorsum=0;
+    for(int i =0;i<n;i++)
+    {
+        xorsum = xorsum ^ arr[i];
+    }
+    int tempxor = xorsum;
+    int pos = -1;
+    int  setbit = 0;
+    while(setbit !=1)
+    {
+        setbit = xorsum ^ 1;
+        pos++;
+        xorsum = xorsum >>1;
+    }
+    //xorsum will become 0 at this point
+    for(int i =0;i<n;i++)
+    {
+        if(getBit(arr[i],pos)){
+            xorsum = xorsum ^ arr[i];
+        }
+    }
+    cout << xorsum <<endl;
+    cout<< (xorsum ^ tempxor)<<endl;
+    return ;
+}
+
+
+int main()
+{
+    int arr[]={1,2,3,4,5,3,2,1};
+    unique(arr,8);
+    return 0;
+}
