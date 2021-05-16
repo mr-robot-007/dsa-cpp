7f44d9ee6a71e55b1dfa28b66a3c18c28892ef45
mr-robot-007 <anujgusain108@gmail.com>
2021-05-16 11:28:50 +0530

    commiting changes

--- bitManipulation/findUnique/unique3.cpp
+++ bitManipulation/findUnique/unique3.cpp
@@ -1,42 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int getBit(int n ,int pos)
+int getBit(int n, int pos)
 {
-    return (( n & (1<<pos))!=0);
+    return ((n & (1 << pos)) != 0);
 }
 
-int  setBit(int n ,int pos)
+int setBit(int n, int pos)
 {
-    return (n | (1<<pos));
+    return (n | (1 << pos));
 }
 
-int unique(int arr[],int n)
+int unique(int arr[], int n)
 {
-    int result =0;
+    int result = 0;
     // traverse for each bit of result/ans(assuming of int 64 bit)
-    for(int i =0; i< 64;i++)
+    for (int i = 0; i < 64; i++)
     {
         int sum = 0;
         //taversing over each element
-        for (int  j =0; j< n;j++)
+        for (int j = 0; j < n; j++)
         {
             // checking whether that particular element has set bit at i or not
-            if(getBit(arr[j],i)){
+            if (getBit(arr[j], i))
+            {
                 sum++;
             }
         }
-        result = setBit(result,sum);
-        
+        if (sum % 3 != 0)
+        {
+            result = setBit(result, i);
+        }
     }
     return result;
 }
 
-
 int main()
 {
-    int arr[]= {1,2,3,4,1,2,3,1,2,3};
-    cout<<unique(arr,10)<<endl;
+    int arr[] = {1, 2, 3, 4, 1, 2, 3, 1, 2, 3};
+    cout << unique(arr, 10) << endl;
 
     return 0;
 }
